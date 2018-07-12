4649c453cedaf91f2edc9783a3f1041953d9d7f4
michellebonat <michelle.bonat@gmail.com>
2018-07-12 08:54:09 -0700

    Split data into training and test using sklearn, 20% of data in test, with random state

--- ml_sklearn_winey.py
+++ ml_sklearn_winey.py
@@ -14,6 +14,22 @@ from sklearn.externals import joblib
 dataset_url = 'http://mlr.cs.umass.edu/ml/machine-learning-databases/wine-quality/winequality-red.csv'
 data = pd.read_csv(dataset_url, sep=';')
 
+# Uncomment this to print first 5 rows of data
 # print data.head()
+
+# Uncomment this to print the data shape
 # print data.shape
-print data.describe()
+
+# Uncomment this to print data overview summary stats
+# print data.describe()
+
+# 3) Split into training and test sets using sklearn
+# Separate target from training features
+y = data.quality
+X = data.drop('quality', axis=1)
+
+# Split data into training and testing sets, 20% of data in test, with random state
+X_train, X_test, y_train, y_test = train_test_split(X, y,
+                                                    test_size=0.2,
+                                                    random_state=123,
+                                                    stratify=y)
